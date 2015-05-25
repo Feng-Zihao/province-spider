3d3ca50468001bcb0fabdd6610aac684e0dc069c
Feng-Zihao <feng.zh.zq@gmail.com>
2015-05-25 23:00:46 +0800

    seems to work

--- pspider.py
+++ pspider.py
@@ -8,12 +8,50 @@ gevent.monkey.patch_all()
 import re
 import requests
 
-r= requests.get('http://www.stats.gov.cn/tjsj/tjbz/tjyqhdmhcxhfdm/2013/index.html')
-r.encoding = 'gb2312'
+url_pool = []
+
+# code, name
+items = []
+
+def getText(url):
+    r = requests.get(url)
+    r.encoding = 'gb2312'
+    return r.text
+
+
+text = getText('http://www.stats.gov.cn/tjsj/tjbz/tjyqhdmhcxhfdm/2013/index.html')
+matches = re.finditer(u"<a href='(?P<code>[^.]+).html'>(?P<name>[^<]+)", text)
+for m in matches:
+    items.append((m.group('code'), m.group('name')))
+    url_pool.append('http://www.stats.gov.cn/tjsj/tjbz/tjyqhdmhcxhfdm/2013/'+
+                    m.group('code')+'.html')
+
+
+def analyse(url):
+    text = getText(url)
+
+    matches = re.finditer(
+                u"<a href='(?P<url>[^']+)'>(?P<code>[0-9]{12})</a></td>"+
+                u"<td><a href='([^.]*).html'>(?P<name>[^<]+)</a>", text)
+    for m in matches:
+        new_url = url[:url.rindex('/')] + '/' + m.group('url')
+        url_pool.append(new_url)
+        items.append((m.group('code'), m.group('name')))
+    else:
+        matches = re.finditer(
+            u"<td>(?P<code>[0-9]{12})</td>"+
+            u"<td>(?P<class>[0-9]{3})</td>"+
+            u"<td>(?P<name>[^<]+)</td>", text)
+        for m in matches:
+            items.append((m.group('code'), m.group('name')))
 
 
 
+while url_pool:
+    print len(url_pool)
+    url = url_pool[0]
+    url_pool = url_pool[1:]
+    analyse(url)
 
-provinces  = re.finditer(u"<a href='(?P<code>[^.]+).html'>(?P<province>[^<]+)", r.text)
-for p in provinces:
-    print p.group('code'), p.group('province')
+for i in sorted(items):
+    print i[0], i[1]
