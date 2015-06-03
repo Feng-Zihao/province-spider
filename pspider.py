bb88c4b682ca152a9fa83b3714f86f97bfaf0fe9
Feng-Zihao <feng.zh.zq@gmail.com>
2015-06-03 11:57:00 +0800

    temp backup

--- pspider.py
+++ pspider.py
@@ -1,15 +1,15 @@
 
 # -*- coding: utf-8 -*-
 
+# -*- coding: utf-8 -*-
 
-import gevent.monkey
-gevent.monkey.patch_all()
+#import gevent.monkey
+#gevent.monkey.patch_all()
 
 import re
 import requests
 
 from sqlalchemy import *
-# -*- coding: utf-8 -*-
 
 from sqlalchemy import Column, create_engine
 from sqlalchemy.orm import sessionmaker, relationship
@@ -17,7 +17,8 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.types import *
 
 
-engine = create_engine('sqlite:///data.sqlite', echo=True)
+engine = create_engine('sqlite:///data.db')
+engine.echo = True
 Session = sessionmaker(bind=engine)
 
 db = Session()
@@ -35,14 +36,12 @@ class District(Base):
     code = Column(String, primary_key=True)
     level = Column(Integer)
     name = Column(String)
-    in_url = Column(String)
+    page_url = Column(String)
     href = Column(String)
     class_code =Column(String)
 
 url_pool = []
 
-# code, name
-items = []
 
 def getText(url):
     r = requests.get(url)
@@ -50,12 +49,27 @@ def getText(url):
     return r.text
 
 
-text = getText('http://www.stats.gov.cn/tjsj/tjbz/tjyqhdmhcxhfdm/2013/index.html')
-matches = re.finditer(u"<a href='(?P<code>[^.]+).html'>(?P<name>[^<]+)", text)
-for m in matches:
-    items.append((m.group('code'), m.group('name')))
-    url_pool.append('http://www.stats.gov.cn/tjsj/tjbz/tjyqhdmhcxhfdm/2013/'+
-                    m.group('code')+'.html')
+def scanHomePage():
+    text = getText('http://www.stats.gov.cn/tjsj/tjbz/tjyqhdmhcxhfdm/2013/index.html')
+    matches = re.finditer(u"<a href='(?P<code>[^.]+).html'>(?P<name>[^<]+)", text)
+    for m in matches:
+        district = District(
+            code=m.group('code'),
+            level=1,
+            name=m.group('name'),
+            page_url='http://www.stats.gov.cn/tjsj/tjbz/tjyqhdmhcxhfdm/2013/index.html',
+            href='http://www.stats.gov.cn/tjsj/tjbz/tjyqhdmhcxhfdm/2013/'+
+                 m.group('code')+'.html'
+        )
+        url_pool = UrlPool(
+            url=district.href,
+            scanned=False)
+        try:
+            db.add(district)
+            db.add(url_pool)
+            db.commit()
+        except:
+            pass
 
 
 def analyse(url):
@@ -77,12 +91,15 @@ def analyse(url):
             items.append((m.group('code'), m.group('name')))
 
 
-while url_pool:
-    print len(url_pool)
-    url = url_pool[0]
+#scanHomePage()
+while true:
+    url_pool = db.query(UrlPool).filter_by(scanned=False).first()
+    if url_pool is None:
+        break
+    url = url_pool.url
     print url
-    url_pool = url_pool[1:]
     analyse(url)
+    url_pool.scanned = True
+    #db.add(url_pool)
+    db.commit()
 
-for i in sorted(items):
-    print i[0], i[1]
